86ca107aea16240a1d25770dba6e10c4114a0a86
Code-By-Amit <amitsaini95471@gmail.com>
2025-03-15 16:02:43 +0530

    fix key issue

--- Frontend/src/pages/ChatPage/ChatArea.jsx
+++ Frontend/src/pages/ChatPage/ChatArea.jsx
@@ -59,7 +59,7 @@ export const ChatArea = ({ currentChatUser, setCurrentChatUser }) => {
 
     useEffect(() => {
         chatEndRef?.current?.scrollIntoView({ behavior: "smooth" });
-    }, [messages,currentChatUser])
+    }, [messages, currentChatUser])
 
 
     useMemo(() => {
@@ -102,27 +102,25 @@ export const ChatArea = ({ currentChatUser, setCurrentChatUser }) => {
                                             )
                                             : (
                                                 Object.keys(groupedMessages).map((dateOfMsg) => {
-                                                    return <div className='space-y-4'>
+                                                    return <div key={dateOfMsg} className='space-y-4'>
                                                         <p className='text-sm text-center text-gray-900 my-4 dark:text-gray-300 font-thin'>{dateOfMsg}</p>
                                                         {
                                                             groupedMessages[dateOfMsg].map((msg) => {
                                                                 const msgTime = new Date(msg.createdAt).toLocaleTimeString('en-Us', { hour: '2-digit', minute: '2-digit', hour12: true })
                                                                 return (
-                                                                    <>
-                                                                        <div key={msg?._id} >
-                                                                            <div className={`flex gap-2 ${msg?.sender?._id === user?._id && "flex-row-reverse"}`}>
-                                                                                <div className='w-8 h-8 ring-1 ring-orange-400 ring-offset-2 overflow-clip rounded-full'>
-                                                                                    <img className='w-full h-full object-cover' src={msg?.sender?.avatar} alt="" />
-                                                                                </div>
-                                                                                
-                                                                                <div className={`text-sm md:text-base px-3 py-2 break-words rounded-t-lg  ${msg?.sender?._id === user?._id ? "rounded-l-lg justify-self-end bg-orange-400 text-white" : "justify-self-start rounded-r-lg bg-gray-300 dark:bg-gray-500 dark:text-gray-200 text-gray-900"} max-w-8/12 md:max-w-1/3 `}>
-                                                                                    {msg?.image && <img src={msg?.image} className=' w-auto h-auto md:h-35' alt='image' />}
-                                                                                    <p>{msg?.message}</p>
-                                                                                </div>
+                                                                    <div key={msg?._id} >
+                                                                        <div className={`flex gap-2 ${msg?.sender?._id === user?._id && "flex-row-reverse"}`}>
+                                                                            <div className='w-8 h-8 ring-1 ring-orange-400 ring-offset-2 overflow-clip rounded-full'>
+                                                                                <img className='w-full h-full object-cover' src={msg?.sender?.avatar} alt="" />
                                                                             </div>
-                                                                            <p className={`text-xs leading-none mb-2 mt-0.5 font-mono dark:text-gray-100 tracking-tighter text-gray-500 ${msg?.sender._id === user?._id ? "text-right" : "text-left"}`}>{msgTime}</p>
+
+                                                                            <div className={`text-sm md:text-base px-3 py-2 break-words rounded-t-lg  ${msg?.sender?._id === user?._id ? "rounded-l-lg justify-self-end bg-orange-400 text-white" : "justify-self-start rounded-r-lg bg-gray-300 dark:bg-gray-500 dark:text-gray-200 text-gray-900"} max-w-8/12 md:max-w-1/3 `}>
+                                                                                {msg?.image && <img src={msg?.image} className=' w-auto h-auto md:h-35' alt='image' />}
+                                                                                <p>{msg?.message}</p>
+                                                                            </div>
                                                                         </div>
-                                                                    </>
+                                                                        <p className={`text-xs leading-none mb-2 mt-0.5 font-mono dark:text-gray-100 tracking-tighter text-gray-500 ${msg?.sender._id === user?._id ? "text-right" : "text-left"}`}>{msgTime}</p>
+                                                                    </div>
                                                                 )
                                                             })
                                                         }
